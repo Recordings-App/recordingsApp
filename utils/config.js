08a009348fddce2bed74cb31dd1bec00e909d99c
Shrirang <deshmukhshrirang01@gmail.com>
2021-04-24 21:03:49 +0530

    Added Utils

--- utils/config.js
+++ utils/config.js
@@ -6,6 +6,9 @@ const DB_USER = process.env.DB_USER;
 const DB_NAME = process.env.DB_NAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const INSTANCE_CONNECTION_NAME = process.env.INSTANCE_CONNECTION_NAME;
+const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN;
+// const JWT_COOKIE_EXPIRES_IN = process.env.JWT_COOKIE_EXPIRES_IN;
 
 module.exports = {
   PORT,
@@ -13,4 +16,6 @@ module.exports = {
   DB_NAME,
   DB_PASSWORD,
   INSTANCE_CONNECTION_NAME,
+  JWT_SECRET,
+  JWT_EXPIRES_IN,
 };
---
+++ utils/middleware.js
@@ -0,0 +1,89 @@
+const AppError = require("../utils/appError");
+const config = require("./config");
+
+const requestLogger = (request, response, next) => {
+  console.log("request method: " + request.method);
+  console.log("request path: " + request.path);
+  console.log("request body: ");
+  console.log(request.body);
+  console.log("--------------------------------------------------");
+  next();
+};
+const unknownEndpoint = (req, res, next) => {
+  return next(new AppError("Unknown endpoint", 404));
+};
+const showError = (err) => {
+  console.log("--------------------------------------------------");
+  console.log(err);
+  console.log("--------------------------------------------------");
+};
+
+const handleDuplicateKeyDB = (err) => {
+  const keys = err.split(" ")[4].split(".");
+  //SQLITE_CONSTRAINT: UNIQUE constraint failed: table_name.column_name
+  const message = `Duplicate ${keys[0]}: ${keys[1]}. Use another value`;
+  return new AppError(message, 400);
+};
+
+const handleJWTError = () =>
+  new AppError("Invalid token. Please log in again!", 401);
+
+const handleJWTExpiredError = () =>
+  new AppError("Your token has expired! Please log in again.", 401);
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error: send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+
+    // Programming or other unknown error: don't leak error details
+  } else {
+    // 1) Log error
+    console.error("ERROR ", err);
+
+    // 2) Send generic message
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong! Please try again in some time.",
+    });
+  }
+};
+
+const errorHandler = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  err.message = err.message || "Something is not right.";
+
+  if (config.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  } else if (config.NODE_ENV === "production") {
+    let error = { ...err };
+    error.message = err.message;
+
+    if (error.message.split(":")[0] === "SQLITE_CONSTRAINT")
+      error = handleDuplicateKeyDB(error.message);
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
+
+    sendErrorProd(error, res);
+  }
+};
+
+module.exports = {
+  requestLogger,
+  unknownEndpoint,
+  errorHandler,
+  showError,
+};
