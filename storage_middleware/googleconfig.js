0237d3786866039d9aee5d16d79e7a2a9e886fcc
yogesh-kansal <yogeshiitbhub@gmail.com>
2021-04-25 00:18:37 +0530

    added cloud strogare functionality

---
+++ storage_middleware/googleconfig.js
@@ -0,0 +1,6 @@
+module.exports={
+    CLOUD_BUCKET: process.env.CLOUD_BUCKET,
+    PROJECT_ID: process.env.CLOUD_PROJECT_ID,
+    keyPath: process.env.FILE_LOCATION,
+    secretKey: process.env.SECRETKEY
+}
